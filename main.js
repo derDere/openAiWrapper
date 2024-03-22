e333ccaef7638d720d4336e34a7d6e63db5c2792
derDere <phillip@deremer.de>
2024-03-22 23:58:01 +0100

    added zoom command arg and light-mode command arg

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 const { app, BrowserWindow } = require('electron')
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+    { name: 'light-mode', alias: 'l', type: Boolean, defaultValue: false },
+    { name: 'zoom', alias: 'z', type: Number, defaultValue: 1.0 }
+];
+
+const filteredArgs = process.argv.slice(1);
+
+const options = commandLineArgs(optionDefinitions, { argv: filteredArgs, partial: true });
+
+options['light-mode'] = !!options['light-mode']
+options['zoom'] = options['zoom'] || 1.0
+
+const bgColor = options['light-mode'] ? '#fff' : '#212121'
+const zoomFactor = options['zoom']
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1600,
     height: 1000,
     icon: 'logo.ico',
-    backgroundColor: '#212121',
+    backgroundColor: bgColor,
     show: false,
   })
   win.on('ready-to-show', () => {
     win.show()
+    win.webContents.setZoomFactor(zoomFactor);
   })
   win.setMenu(null)
   win.loadURL('https://chat.openai.com/')
@@ -18,3 +35,6 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow()
 })
+
+
+//*/
